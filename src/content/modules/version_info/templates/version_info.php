d50f334453190c14c3d9769a53a0f03f5283e03d
Ulrich Schmidt <admin@deruli.de>
2017-07-08 13:13:08 +0200

    version 1.1

--- src/content/modules/version_info/templates/version_info.php
+++ src/content/modules/version_info/templates/version_info.php
@@ -2,7 +2,7 @@
 $acl = new ACL ();
 if ($acl->hasPermission ( "version_info" )) {
 	?>
-	| UliCMS <?php Template::escape(cms_version());?> | PHP  <?php Template::escape(phpversion());?> |
+	- UliCMS <?php Template::escape(cms_version());?> | PHP  <?php Template::escape(phpversion());?> |
 	 <?php Template::escape($_SERVER['SERVER_SOFTWARE']);?> | MySQL <?php Template::escape(Database::getServerVersion());?>
 </small>
 <?php
